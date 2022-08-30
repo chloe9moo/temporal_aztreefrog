916f989bd3ff0d39bb66141db78ae4c21004ee9a
chloe9moo <chloe9mo@gmail.com>
2022-08-30 15:46:43 -0400

    august 30 updates

--- code/01a_DataPrep.R
+++ code/01a_DataPrep.R
@@ -9,10 +9,14 @@ library(tidyverse); library(readxl); library(pegas)
 
 PATH <- "/home/chloe9mo/Documents/Projects/temporal_aztreefrog"
 PATH_15 <- paste0(PATH, "/microsat_data/2015")
+PATH_18 <- paste0(PATH, "/microsat_data/2018")
 PATH_19 <- paste0(PATH, "/microsat_data/2019")
+PATH_21 <- paste0(PATH, "/microsat_data/2021")
 
 aztf19 <- lapply(list.files(path = PATH_19, pattern = "2019_Mix*", full.names = TRUE), read.csv)
 aztf15 <- lapply(list.files(path = PATH_15, pattern = "*Alleles.csv", full.names = TRUE), read.csv)
+aztf18 <- lapply(list.files(path = PATH_18, pattern = "*Alleles.csv", full.names = T), read.csv)
+aztf21 <- lapply(list.files(path = PATH_21, pattern = "*Alleles.csv", full.names = T), read.csv)
 
 mims <- read_excel(paste0(PATH_15, "/Mims2016_GenotypeData.xlsx"), sheet = 2)
 
@@ -21,7 +25,13 @@ aztf19 <- lapply(aztf19, function (x) { cbind(indiv = gsub("([0-9]+)_.*", "\\1",
 aztf19 <- lapply(aztf19, function (x) { x[!(names(x) %in% c("Name"))] })
 
 aztf15 <- lapply(aztf15, function (x) { cbind(indiv = gsub("([0-9]+)_.*", "\\1", x$Name), x) })
-aztf15<- lapply(aztf15, function (x) { x[!(names(x) %in% c("Name"))] })
+aztf15 <- lapply(aztf15, function (x) { x[!(names(x) %in% c("Name"))] })
+
+aztf21 <- lapply(aztf21, function (x) { cbind(indiv = gsub("([0-9]+)_.*", "\\1", x$Name), x) })
+aztf21 <- lapply(aztf21, function (x) { x[!(names(x) %in% c("Name"))] })
+
+aztf18 <- lapply(aztf18, function (x) { cbind(indiv = gsub("([0-9]+)_.*", "\\1", x$Name), x) })
+aztf18 <- lapply(aztf18, function (x) { x[!(names(x) %in% c("Name"))] })
 
 #-----------------------------#
 #### 2015 data comparisons ####
@@ -215,23 +225,24 @@ rr <- aztf19[[4]]
 aztf2019c <- right_join(rr, aztf2019) #doesn't seem to be any differences in the rerun plate
 
 #add in population info
-pops <- read.csv(paste0(PATH, sep = "/", "multi-year_HYWR_sample_tracksheet.csv"))
-pops <- pops[,c(1,2,4)]
-names(pops) <- c("indiv", "age", "pop")
-pops[199,1] <- "19-074"
+pops <- read.csv(paste0(PATH, "/multiyear_hywr_tracking.csv"))
+pops <- pops %>% select(Individual.ID, Pond.Number, Type) %>% rename(indiv = Individual.ID, pop = Pond.Number, age = Type)
+pops[pops[, c("indiv")] == "19-074*",]$indiv <- "19-074"
 pops <- pops %>% 
-  mutate(age = case_when(age == 'Buccal' ~ 'Adult', age == 'Tail' ~ 'Larvae', age == 'Toe' ~ 'Adult', TRUE ~ age),
-         pop = case_when(pop == 'T9' ~ '7',
-                         pop == 'T8' ~ '6',
-                         pop == 'T11' ~ '9',
-                         pop == 'FS4636' ~ '4',
-                         pop == 'UGCP' ~ '8',
-                         pop == 'T4' ~ '11',
-                         pop == 'A5' ~ '12',
-                         pop == 'Unknown' ~ '15',
-                         pop == 'Double Tank Up-Canelo Hills' ~ '14',
-                         pop == 'T10' ~ '13',
-                         T ~ pop)) %>%
+  mutate(age = case_when(age == 'Buccal' ~ 'Adult', age == 'Tail' ~ 'Larvae', age == 'Toe' ~ 'Adult',
+                         age == 'tail' ~ 'Larvae', age == 'toe' ~ 'Adult',  age == 'toe ' ~ 'Adult', TRUE ~ age),
+         # pop = case_when(pop == 'T9' ~ '7',
+         #                 pop == 'T8' ~ '6',
+         #                 pop == 'T11' ~ '9',
+         #                 pop == 'FS4636' ~ '4',
+         #                 pop == 'UGCP' ~ '8',
+         #                 pop == 'T4' ~ '11',
+         #                 pop == 'A5' ~ '12',
+         #                 pop == 'Unknown' ~ '15',
+         #                 pop == 'Double Tank Up-Canelo Hills' ~ '14',
+         #                 pop == 'T10' ~ '13',
+         #                 T ~ pop)
+         ) %>%
   filter(age %in% c('Adult', 'Larvae'))
 
 aztf2019 <- merge(aztf2019, pops, by = "indiv")
@@ -243,14 +254,113 @@ aztf2019[is.na(aztf2019)] <- '-1' #match nas to 2015
 
 write.csv(aztf2019, file = paste0(PATH_19, "/AZTF19_GenotypeData.csv"), row.names = FALSE)
 
-#-----------------------------------#
-#### combine 2015 + 2019, format ####
-#-----------------------------------#
+#--------------------------#
+#### reformat data 2021 ####
+#--------------------------#
+
+#remove negatives
+aztf21 <- lapply(aztf21, function (x) { x[!grepl("neg", x$indiv),] })
+ml <- lapply(aztf21, function (x) colnames(x)) %>% #get loci names
+  unlist() %>% as.data.frame() %>% rename(loci = '.') %>% #create dataframe
+  mutate(loci = gsub("Hwri(.+)\\.\\.\\.[1-2]", "\\1", loci)) %>% unique() %>% #get only the numbers of the loci
+  filter(loci != 'indiv')
+
+#set dataframe to populate with microsat data
+aztf2021 <- lapply(aztf21, function (x) x %>% select(indiv)) %>% bind_rows() %>% unique()
+
+#consolidate alleles into single dataframe
+for (i in 1:3) {
+  for(y in 1:nrow(ml)) {
+    
+    l <- ml[y,1]
+    aztf2021 <- aztf21[[i]] %>% select(indiv, contains(l)) %>% unique() %>% na.omit() %>% right_join(., aztf2021)
+    
+  }
+}
+
+
+#add in population info
+# pops <- read.csv(paste0(PATH, "/updated_multiyear_hywr_tracksheet.csv"))
+# pops <- pops %>% 
+#   filter(Year.Sampled == "2021") %>%
+#   mutate(indiv = str_extract(Individual.ID, "21_.*")) %>%
+#   rename(year = Year.Sampled, pop = Pond.Number, age = Type) %>%
+#   dplyr::select(indiv, age, pop, year) %>%
+#   mutate(indiv = str_replace(indiv, "_", "-"),
+#          age = case_when(age == 'toe' ~ 'Adult', age == 'toe ' ~ 'Adult', age == 'tail' ~ 'Larvae', TRUE ~ age)) %>%
+#   filter(indiv %in% aztf2021$indiv)
+aztf2021 <- merge(aztf2021, pops, by = "indiv")
+aztf2021$year <- 2021
+aztf2021 <- aztf2021[,c(1,36,37,38,2:35)]
+
+colnames(aztf2021) <- sub("\\.\\.\\.", "_", colnames(aztf2021)) #matching names to 2015 data
+names(aztf2021) <- tolower(names(aztf2021))
+aztf2021[is.na(aztf2021)] <- '-1' #match nas to 2015
+
+write.csv(aztf2021, file = paste0(PATH_21, "/AZTF21_GenotypeData.csv"), row.names = FALSE)
+
+#--------------------------#
+#### reformat data 2018 ####
+#--------------------------#
+
+#remove negatives
+aztf18 <- lapply(aztf18, function (x) { x[!grepl("neg", x$indiv),] })
+ml <- lapply(aztf18, function (x) colnames(x)) %>% #get loci names
+  unlist() %>% as.data.frame() %>% rename(loci = '.') %>% #create dataframe
+  mutate(loci = gsub("Hwri(.+)\\.\\.\\.[1-2]", "\\1", loci)) %>% unique() %>% #get only the numbers of the loci
+  filter(loci != 'indiv')
+
+#set dataframe to populate with microsat data
+aztf2018 <- lapply(aztf18, function (x) x %>% select(indiv)) %>% bind_rows() %>% unique()
+
+#consolidate alleles into single dataframe
+for (i in 1:3) {
+  for(y in 1:nrow(ml)) {
+    
+    l <- ml[y,1]
+    aztf2018 <- aztf18[[i]] %>% select(indiv, contains(l)) %>% unique() %>% na.omit() %>% right_join(., aztf2018)
+    
+  }
+}
+
+
+#add in population info
+# pops <- read.csv(paste0(PATH, "/updated_multiyear_hywr_tracksheet.csv"))
+# pops <- pops %>% 
+#   filter(Year.Sampled == "2018") %>%
+#   mutate(indiv = str_extract(Individual.ID, "18_.*")) %>%
+#   rename(year = Year.Sampled, pop = Pond.Number, age = Type) %>%
+#   dplyr::select(indiv, age, pop, year) %>%
+#   mutate(indiv = str_replace(indiv, "_", "-"),
+#          age = case_when(age == 'toe' ~ 'Adult', age == 'toe ' ~ 'Adult', age == 'tail' ~ 'Larvae', TRUE ~ age)) %>%
+#   filter(indiv %in% aztf2018$indiv)
+aztf2018 <- merge(aztf2018, pops, by = "indiv")
+aztf2018$year <- 2018
+aztf2018 <- aztf2018[,c(1,36,37,38,2:35)]
+
+colnames(aztf2018) <- sub("\\.\\.\\.", "_", colnames(aztf2018)) #matching names to 2015 data
+names(aztf2018) <- tolower(names(aztf2018))
+aztf2018[is.na(aztf2018)] <- '-1' #match nas to 2015
+
+write.csv(aztf2018, file = paste0(PATH_18, "/AZTF18_GenotypeData.csv"), row.names = FALSE)
+
+
+#-----------------------------------------#
+#### combine 2015, 2018, 2019, 2021 - format ####
+#-----------------------------------------#
 #read in data
-aztf2019 <- read.csv(paste0(PATH_19, "/AZTF19_GenotypeData.csv"))
-aztf2015.v2 <- read.csv(paste0(PATH_15, "/Mims2016_GenotypeData_Updated.csv")) %>% select(-X)
-aztf2019[,c(5:38)] <- apply(aztf2019[,c(5:38)], 2, as.numeric)
-aztf_loci <- full_join(aztf2015.v2, aztf2019)
+aztf2019 <- read.csv(paste0(PATH_19, "/AZTF19_GenotypeData.csv")) %>% mutate(pop = as.character(pop))
+aztf2015.v2 <- read.csv(paste0(PATH_15, "/Mims2016_GenotypeData_Updated.csv")) %>% select(-X) %>%
+  mutate(old.pop = as.character(pop)) %>%
+  mutate(pop = case_when(old.pop == '6'~'9', old.pop == '9'~'6', T ~ old.pop)) %>%
+  select(-old.pop)
+aztf2015.v2 <- aztf2015.v2 %>% 
+  mutate(across(starts_with("hwri"), ~ ifelse(.x < 10, -1, .x))) #change the incorrect neg. values
+aztf2021 <- read.csv(paste0(PATH_21, "/AZTF21_GenotypeData.csv")) %>% mutate(pop = as.character(pop))
+aztf2018 <- read.csv(paste0(PATH_18, "/AZTF18_GenotypeData.csv")) %>% mutate(pop = as.character(pop))
+# aztf2019[,c(5:38)] <- apply(aztf2019[,c(5:38)], 2, as.numeric)
+
+aztf_loci <- aztf2015.v2 %>% full_join(aztf2019) %>% full_join(aztf2021) %>% full_join(aztf2018)
 
 write.csv(aztf_loci, file = paste0(PATH, "/microsat_data/combined_aztf_genotypes.csv"), row.names = FALSE)
 
@@ -260,6 +370,55 @@ microsat <- aztf_loci[,c(5:38)] #separate out loci
 microsat <- pegas::alleles2loci(microsat) #convert
 colnames(microsat) <- sub("_1", "", colnames(microsat))
 microsat[c("ID", "pop", "age", "year")] <- aztf_loci[,c(1:4)]
-microsat <- microsat[,c(18:21,1:17)]
+microsat <- microsat %>% mutate(year_pop = paste0(year, "_", pop)) #add in another grouping option
+microsat[microsat == "2018_16"] <- "2019_16"
+microsat <- microsat[,c(18:22,1:17)]
 
-write.csv(microsat, file = paste0(PATH, "/microsat_data/combined_aztf_loci_", Sys.Date(), ".csv"), row.names = FALSE)
+write.csv(microsat, file=paste0(PATH, "/microsat_data/combined_aztf_loci.csv"), row.names = F)
+
+# remove siblings ----
+aztf <- read.csv(paste0(PATH, "/microsat_data/combined_aztf_loci.csv"))
+sibs <- read.csv(paste0(PATH, "/multiyear_hywr_tracking.csv"))
+
+sibs <- sibs %>% select(Individual.ID, Sibling.Action)
+k.sibs <- sibs %>% filter(Sibling.Action == "keep") %>% select(Individual.ID) %>% rename(ID = Individual.ID)
+
+aztf.s <- aztf %>% filter(ID %in% k.sibs$ID)
+
+write.csv(aztf.s, file=paste0(PATH, "/microsat_data/combined_aztf_loci_nosibs.csv"), row.names = F)
+
+#check which sibs weren't removed that Meryl removed
+# mims <- read_excel(paste0(PATH_15, "/Mims2016_GenotypeData.xlsx"), sheet = 3)
+# aztf.f <- read.csv(paste0(PATH, "/microsat_data/filtered_aztf_loci.csv"))
+# mims <- mims %>% select(Sample) %>% rename(ID = Sample)
+# aztf.s %>% filter(year == '2015' & !ID %in% mims$ID) %>% select(ID) #samples I kept, Meryl didn't
+# mims %>% filter(!ID %in% aztf.s$ID)
+
+# NeEstimator prep ----
+library(adegenet)
+aztf <- read.csv(paste0(PATH,"/microsat_data/filtered_aztf_loci.csv"))
+microsat <- aztf %>% select(contains("hwri"))
+# microsat <- df2genind(microsat, sep = "/", ind.names = aztf$ID, NA.char = "-1/-1")
+microsat <- microsat %>% 
+  mutate(across(everything(), ~gsub("/", "", .x))) %>% 
+  mutate(across(everything(), ~gsub("-1-1", "000000", .x)))
+microsat["pop"] <- aztf$year_pop
+microsat <- microsat[,c(18,1:17)]
+microsat <- arrange(microsat, pop)
+
+write.table(microsat, file=paste0(PATH,"/microsat_data/Ne_loci_original.txt"), sep = '\t', row.names = F)
+
+# STRUCTURE prep ----
+aztf <- read.csv(paste0(PATH,"/microsat_data/filtered_aztf_loci.csv"))
+str <- aztf %>% select(-year_pop, -year, -age)
+ind_aztf_pop <- df2genind(str[,c(3:19)], sep = "/", ind.names = str$ID, NA.char = "-1/-1")
+ind_aztf_pop@pop <- as.factor(str$pop) #assign populations
+str <- genind2df(ind_aztf_pop, sep = "/", usepop = T, oneColPerAll = T)
+str <- str %>% mutate(Label = row.names(str), Pop = pop) %>% select(-pop)
+str <- str[,c(35,36,1:34)]
+str <- str %>% select(-Label)
+names(str) <- gsub("\\.1", "", names(str))
+names(str) <- gsub("\\.2", "", names(str))
+str[str=="NA"] <- -1
+
+write.table(str, file=paste0(PATH,"/microsat_data/structure_loci.txt"), sep = '\t', row.names = F, quote = F)
---
+++ code/01c_DataLoad.R
@@ -0,0 +1,62 @@
+# loading and preparing the dataset to be sourced at the beginning of each script
+#which year grouping do you want?
+
+#load in data
+aztf <- read.csv(paste0(PATH, "/microsat_data/final_aztf_loci_nosibs.csv"))
+aztf <- aztf %>% mutate(year = ifelse(year == 2018, 2019, year)) %>%
+  mutate(year = ifelse(year == 2015, 2014, year))
+coord <- read.csv(paste0(PATH, "/pond_coordinates.csv"))
+pop.coord <- coord %>% dplyr::select(pop, lon, lat) %>% unique() %>% remove_rownames() %>% column_to_rownames("pop")
+
+ind_aztf_pop <- df2genind(aztf[,c(6:22)], sep = "/", ind.names = aztf$ID, NA.char = "-1/-1")
+ind_aztf_pop@pop <- as.factor(aztf$pop) #assign populations
+ind_aztf_pop$other$xy <- subset(pop.coord, rownames(pop.coord) %in% aztf$pop) #assign coordinates
+
+ind_aztf_yr <- df2genind(aztf[,c(6:22)], sep = "/", ind.names = aztf$ID, NA.char = "-1/-1")
+ind_aztf_yr@pop <- as.factor(aztf$year)
+# set coordinates in the script
+
+o.2015 <- aztf %>% filter(year == '2014')
+ind2015 <- df2genind(o.2015[,c(6:22)], sep = "/", ind.names = o.2015$ID, NA.char = "-1/-1")
+ind2015@pop <- as.factor(o.2015$pop)
+ind2015$other$xy <- subset(pop.coord, rownames(pop.coord) %in% o.2015$pop)
+
+o.2019 <- aztf %>% filter(year == '2019')
+ind2019 <- df2genind(o.2019[,c(6:22)], sep = "/", ind.names = o.2019$ID, NA.char = "-1/-1")
+ind2019@pop <- as.factor(o.2019$pop)
+ind2019$other$xy <- subset(pop.coord, rownames(pop.coord) %in% o.2019$pop)
+
+o.2021 <- aztf %>% filter(year == '2021')
+ind2021 <- df2genind(o.2021[,c(6:22)], sep = "/", ind.names = o.2021$ID, NA.char = "-1/-1")
+ind2021@pop <- as.factor(o.2021$pop)
+ind2021$other$xy <- subset(pop.coord, rownames(pop.coord) %in% o.2021$pop)
+
+#make pop data
+pop_aztf_pop <- genind2genpop(ind_aztf_pop, quiet = T) #global
+pop_aztf_yr <- genind2genpop(ind_aztf_yr, quiet = T)
+pop2015 <- genind2genpop(ind2015, quiet = T)
+pop2019 <- genind2genpop(ind2019, quiet = T)
+pop2021 <- genind2genpop(ind2021, quiet = T)
+
+#make list
+ind.list <- list(ind_aztf_pop, ind_aztf_yr, ind2015, ind2019, ind2021)
+ind.names <- list("All Yrs - Indiv x Pop", "All Yrs - Indiv x Year", "Indiv 2014", "Indiv 2019", "Indiv 2021")
+pop.list <- list(pop_aztf_pop, pop_aztf_yr, pop2015, pop2019, pop2021)
+pop.names <- list("All Yrs - Pop x Pop", "All Yrs - Pop x Year", "Pop 2014", "Pop 2019", "Pop 2021")
+
+#color palette
+aztf.pal <- c("1" = "#28231D", 
+              "3" = "#5E2D30", 
+              "4" = "#008E90", 
+              "6" = "#5cc589", 
+              "7" = "#1C77A3", 
+              "8" = "#C5A387",
+              "9" = "#67B8D6", 
+              "10" = "#E9D097", 
+              "16" = "#283F79")
+
+#remove extras
+rm(ind_aztf_pop, ind_aztf_yr, ind2015, ind2019, ind2021, 
+   pop_aztf_pop, pop_aztf_yr, pop2015, pop2019, pop2021, 
+   o.2015, o.2019, o.2021,
+   pop.coord)
